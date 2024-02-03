30ed1eb66549cd71df091b970492ef843801507d
MellowByte <vivobinjai93@gmail.com>
2024-02-04 02:21:56 +0700

    project

---
+++ latihan-17.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+	{
+		float luas;
+		int t, a;
+		
+		cout<<"Masukkan Alas : ";
+		cin>>a;
+		cout<<"Masukkan tinggi : ";
+		cin>>t;
+		cout<<endl;
+		
+		luas=0.5*a*t;
+		cout<<"Luas Segitiga = "<<luas<<endl;
+	getch();
+	}
